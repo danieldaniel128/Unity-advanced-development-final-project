b3c7fed8e4c4e9aa4529c019241db9a9eb248d70
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-06-19 22:04:22 +0300

    level generation. edge collider points through code

--- Assets/Scripts/Tools/GenerateLevel.cs
+++ Assets/Scripts/Tools/GenerateLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -16,20 +17,33 @@ public class GenerateLevel : MonoBehaviour
     public GameObject GroundPrefab;
     public Transform LevelParent;
 
+    public EdgeCollider2D EdgeCollider;
+
+    List<Vector2> EdgeColliderPoints;
+
     [ContextMenu("Generate Level")]
     void CreateLevel()
     {
+        EdgeColliderPoints = new List<Vector2>();
         TileMap = new int[Width, Height];
         TileMapHeights = new int[Width];
         //Debug.Log($"Width: {Width}");
         //Debug.Log($"Height: {Height}");
+            EdgeColliderPoints.Add(new Vector2(-GroundPrefab.GetComponent<SpriteRenderer>().size.x / 2, -GroundPrefab.GetComponent<SpriteRenderer>().size.y / 2));
         for (int x = 0; x < Width; x++)
         {
             float num1 = Random.Range(289, 10000);
             int perlinNoiceHeight = Mathf.RoundToInt(Mathf.PerlinNoise((x + 1) * Width * Height * num1, num1 * 2 / 7) * Height/2);
             Debug.Log($"<color=red>perlinNoiceHeight: {perlinNoiceHeight}</color>");
             TileMapHeights[x] = perlinNoiceHeight;
+            perlinNoiceHeight--;
+            EdgeColliderPoints.Add(new Vector2(x - GroundPrefab.GetComponent<SpriteRenderer>().size.x / 2, perlinNoiceHeight + GroundPrefab.GetComponent<SpriteRenderer>().size.y / 2));
+            EdgeColliderPoints.Add(new Vector2(x + GroundPrefab.GetComponent<SpriteRenderer>().size.x / 2, perlinNoiceHeight + GroundPrefab.GetComponent<SpriteRenderer>().size.y / 2));
         }
+        EdgeColliderPoints.Add(new Vector2(Width-1+GroundPrefab.GetComponent<SpriteRenderer>().size.x / 2, -GroundPrefab.GetComponent<SpriteRenderer>().size.y / 2));
+        //EdgeColliderPoints.Add(new Vector2(-GroundPrefab.GetComponent<SpriteRenderer>().size.x / 2, -GroundPrefab.GetComponent<SpriteRenderer>().size.y / 2));
+        EdgeCollider.SetPoints(EdgeColliderPoints);
+
 
         for (int x = 0; x < Width; x++)
         {
