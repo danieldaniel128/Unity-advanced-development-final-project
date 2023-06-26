9e5c2fb3bdd762200bf79fcd4134b6089fd710b0
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-06-26 20:33:00 +0300

    tile prefabs can connect to references

--- Assets/Scripts/Data/PrefabConnecter.cs
+++ Assets/Scripts/Data/PrefabConnecter.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class PrefabConnecter : MonoBehaviour 
 {
-    [SerializeField] CellStateEnum[] PossibleToConnectStates; 
+    //[SerializeField] CellStateEnum[] PossibleToConnectStates; 
+    [SerializeField] List<GameObject> TopCornerLeft;
+    [SerializeField] List<GameObject> TopCornerMid;
+    [SerializeField] List<GameObject> TopCornerRight;
+    [SerializeField] List<GameObject> MidLeft;
+    [SerializeField] List<GameObject> MidRight;
+    [SerializeField] List<GameObject> BottomCornerMid;
+    [SerializeField] List<GameObject> BottomCornerRight;
+    [SerializeField] List<GameObject> BottomCornerLeft;
 }
+/*
+
+TopCornerLeft
+TopCornerMid
+TopCornerRight
+BottomCornerMid
+BottomCornerRight
+BottomCornerLeft
+MidLeft
+MidRight
+MidMidlle 
+
+ */
